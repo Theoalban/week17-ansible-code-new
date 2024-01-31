075c6d8da3e5ccf3a0bdce2578406eb4f2d0741a
Theoalban <kemajoutheodore@hotmail.com>
2024-01-31 05:51:01 -0600

    new change

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#install amazon ansible-ec2 plugin
+
+ansible-galaxy collection install amazon.aws
+# # install ansible with python3
+sudo yum update -y
+sudo amazon-linux-extras install python3.8 -y
+sudo pip3.8 install ansible
+
+# #install boto3 and botocore
+sudo pip3.8 install boto3 botocore awscli
+#change terminal color
+echo "PS1='\e[1;32m\u@\h \w$ \e[m'" >> /home/ec2-user/.bash_profile
+
+sudo yum install git -y
+mkdir -p /home/ec2-user/ansible-dev
+git clone https://github.com/Theoalban/week17-ansible-code.git
+
+cp -r week17-ansible-code/* /home/ec2-user/ansible-dev
+
+rm -rf week17-ansible-code
+
+exit 0
